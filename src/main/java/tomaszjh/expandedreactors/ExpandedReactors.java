6e5507655085bcb46033d5fc9d7f1124873069a3
TomaszJH <tomjh9857@gmail.com>
2021-05-17 18:29:31 +0100

    Java source setup phase 1

---
+++ src/main/java/tomaszjh/expandedreactors/ExpandedReactors.java
@@ -0,0 +1,25 @@
+package tomaszjh.expandedreactors;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.*;
+
+@Mod(modid = Global.MOD_ID, name = Global.MOD_NAME, version = Global.VERSION, dependencies = Global.DEPENDENCIES)
+public class ExpandedReactors {
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+
+}
---
+++ src/main/java/tomaszjh/expandedreactors/proxy/ClientProxy.java
@@ -0,0 +1,5 @@
+package tomaszjh.expandedreactors.proxy;
+
+public class ClientProxy extends CommonProxy {
+
+}
