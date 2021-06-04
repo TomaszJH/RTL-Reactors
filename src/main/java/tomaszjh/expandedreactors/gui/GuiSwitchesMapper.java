5863dc9664f4dac7bd5f51e4c5a0c4685d738505
TomaszJH <tomjh9857@gmail.com>
2021-06-05 00:03:27 +0100

    More Gui Work

---
+++ src/main/java/tomaszjh/expandedreactors/gui/GuiSwitchesMapper.java
@@ -0,0 +1,20 @@
+package tomaszjh.expandedreactors.gui;
+
+public enum GuiSwitchesMapper {
+    TYPE_A_NORTH,
+    TYPE_A_NEAST,
+    TYPE_A_EAST,
+    TYPE_A_SEAST,
+    TYPE_A_SOUTH,
+    TYPE_A_SWEST,
+    TYPE_A_WEST,
+    TYPE_A_NWEST,
+    TYPE_B_NORTH,
+    TYPE_B_NEAST,
+    TYPE_B_EAST,
+    TYPE_B_SEAST,
+    TYPE_B_SOUTH,
+    TYPE_B_SWEST,
+    TYPE_B_WEST,
+    TYPE_B_NWEST,
+}
--- src/main/java/tomaszjh/expandedreactors/handler/RegistrationHandler.java
+++ src/main/java/tomaszjh/expandedreactors/handler/RegistrationHandler.java
@@ -5,8 +5,10 @@ import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import tomaszjh.expandedreactors.block.ERBlock;
+import tomaszjh.expandedreactors.block.fluid.ERBlockFluid;
 import tomaszjh.expandedreactors.item.ERItem;
 
 import java.util.ArrayList;
